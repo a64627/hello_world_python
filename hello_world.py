67b7aec88abb799d08970aafc0603cee3fb35fe3
Goncalo Oliveira <a64627@ualg.pt>
2022-06-09 04:54:57 +0000

    Changes to hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,12 @@
 def hello():
     return 'Hello World!'
+
+def print_hello():
+    print(hello())
     
-print(hello())
+def name():
+    name = input("Enter your name: ")
+    print("Hello", name + "!")
+    
+print_hello()
+name()
